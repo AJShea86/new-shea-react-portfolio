6cacdb80c4aca5b73d7f6e6af55317cc85117ba4
Andrew S <andrewsheamls@gmail.com>
2022-04-14 20:21:56 -0600

    section title color changes now

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,12 @@ import React, { useState } from 'react';
 
 const Contact = ()=>{
 
+
+
+
+
+
+
   const [name, setName] = useState('')
 
   const [email, setEmail] = useState('')
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,21 @@ const Header = (props)=>{
     color:props.active === "Education"? "red" : null
   
   }
+  const contactStyle = {
+    color:props.active === "Contact"? "red" : null
+  
+  }
+
+  const portfolioStyle = {
+    color:props.active === "Portfolio"? "red" : null
+  
+  }
+  const backgroundStyle = {
+    color:props.active === "Background"? "red" : null
+  
+  }
+
+
 
 
     return  (<nav className="fixed-top navbar navbar-expand-lg navbar-light nav " >
@@ -22,13 +37,13 @@ const Header = (props)=>{
           <a style={educationStyle} className="nav-link" onClick={()=>props.changeLink('Education')} href="#education">Education</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" onClick={()=>props.changeLink('Background')} href="#background">Background</a>
+          <a style={backgroundStyle} className="nav-link" onClick={()=>props.changeLink('Background')} href="#background">Background</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" onClick={()=>props.changeLink('Contact')} href="#contactme">Contact Me</a>
+          <a style={contactStyle} className="nav-link" onClick={()=>props.changeLink('Contact')} href="#contactme">Contact Me</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" onClick={()=>props.changeLink('Portfolio')} href="#portfolio">Portfolio</a>
+          <a style={portfolioStyle} className="nav-link" onClick={()=>props.changeLink('Portfolio')} href="#portfolio">Portfolio</a>
         </li>
       </ul>
       <a type="button"   className="btn btn-outline-dark" href="Shea-Resume (10).pdf" download >Download Resume</a>
